3c697a60e590940d31edd02c95fe336357e08175
John-Henry Liberty <johnhenry.liberty@gmail.com>
2018-07-19 08:30:30 -0500

    compiles and remove gitignore line for lib since were not publishing to npm

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { PersistedQueryNetworkInterface, addPersistedQueries } from './src/network_interface/ApolloNetworkInterface';
+import { OutputMap, getQueryDocumentKey } from './src/common';
+export { PersistedQueryNetworkInterface, addPersistedQueries, getQueryDocumentKey, OutputMap, };
